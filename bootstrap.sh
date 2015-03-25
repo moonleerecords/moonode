896176562bbea2e0789478bef8b89bbadc2fe156
drazen <peric.drazhen@gmail.com>
2015-03-25 19:17:03 +0100

    Updated bootstrap.sh (imagemagick)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # The output of all these installation steps is noisy. With this utility
 # the progress report is nice and concise.
 function install {
@@ -24,23 +25,24 @@ install SQLite sqlite3 libsqlite3-dev
 install memcached memcached
 install Redis redis-server
 install RabbitMQ rabbitmq-server
+install imagemagick
 
 install PostgreSQL postgresql postgresql-contrib libpq-dev
 sudo -u postgres createuser --superuser vagrant
 sudo -u postgres createdb -O vagrant activerecord_unittest
 sudo -u postgres createdb -O vagrant activerecord_unittest2
 
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-install MySQL mysql-server libmysqlclient-dev
-mysql -uroot -proot <<SQL
-CREATE USER 'rails'@'localhost';
-CREATE DATABASE activerecord_unittest  DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-CREATE DATABASE activerecord_unittest2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON activerecord_unittest.* to 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON activerecord_unittest2.* to 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON inexistent_activerecord_unittest.* to 'rails'@'localhost';
-SQL
+# debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+#debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+#install MySQL mysql-server libmysqlclient-dev
+#mysql -uroot -proot <<SQL
+#CREATE USER 'rails'@'localhost';
+#CREATE DATABASE activerecord_unittest  DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+#CREATE DATABASE activerecord_unittest2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+#GRANT ALL PRIVILEGES ON activerecord_unittest.* to 'rails'@'localhost';
+#GRANT ALL PRIVILEGES ON activerecord_unittest2.* to 'rails'@'localhost';
+#GRANT ALL PRIVILEGES ON inexistent_activerecord_unittest.* to 'rails'@'localhost';
+#SQL
 
 install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
 install 'ExecJS runtime' nodejs
